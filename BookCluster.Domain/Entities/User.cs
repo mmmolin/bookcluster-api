e1d90252f83b144d62bec44926ffbfbe824217ff
unknown <mmmolin78@gmail.com>
2020-01-14 22:05:40 +0100

    Add UserRepository

--- BookCluster.Domain/Entities/User.cs
+++ BookCluster.Domain/Entities/User.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using BookCluster.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BookCluster.Domain.Entities
 {
-    public class User
+    public class User : IHaveId
     {
+        [Key]
         public int Id { get; set; }
         public string Password { get; set; }
         public string Username { get; set; }
